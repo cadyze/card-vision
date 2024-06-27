c783335e1b7a8406dae5267a56a42ca641914fe5
Jacob Cadavez <66289410+cadyze@users.noreply.github.com>
2024-06-27 13:22:40 -0400

    important file upload from laptop

---
+++ yolo-data-fix.py
@@ -0,0 +1,19 @@
+import supervision as sv
+
+from roboflow import Roboflow
+rf = Roboflow(api_key="WoJcpr1rwlUPyPJivcm2")
+
+# grab our data
+project = rf.workspace("cadyzedevelopmenthub").project("card-detection-zk7wu")
+version = project.version(8)
+dataset = version.download("yolov8")
+
+# for each image, load YOLO annotations and require mask format for each
+for subset in ["train", "test", "valid"]:
+    ds = sv.DetectionDataset.from_yolo(
+        images_directory_path=f"{dataset.location}/{subset}/images",
+        annotations_directory_path=f"{dataset.location}/{subset}/labels",
+        data_yaml_path=f"{dataset.location}/data.yaml",
+        force_masks=True
+    )
+    ds.as_yolo(annotations_directory_path=f"{dataset.location}/{subset}/labels")
