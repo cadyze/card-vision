c969347b60a6bf0b1daf4e254c80864ff5e86a7f
Jacob Cadavez <jacobcad09@gmail.com>
2024-06-30 21:47:23 -0400

    Achieved 90% mAP accuracy

---
+++ card-vision-main.py
@@ -0,0 +1,45 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+# Load a model
+l_path = r'runs/detect/train21/weights/best.pt'
+model = YOLO(r'runs/detect/train21/weights/best.pt')  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+# results = model(["datasets/Card-Detection-8/test/images/021407554_jpg.rf.d68568480c4c6d78df79181201aebfe0.jpg"])  # return a list of Results objects
+
+# # Process results list
+# for result in results:
+#     # boxes = result.boxes  # Boxes object for bounding box outputs
+#     masks = result.masks  # Masks object for segmentation masks outputs
+#     keypoints = result.keypoints  # Keypoints object for pose outputs
+#     probs = result.probs  # Probs object for classification outputs
+#     # obb = result.obb  # Oriented boxes object for OBB outputs
+#     result.show()  # display to screen
+
+def preprocess_image(image, input_shape):
+    # Resize image to the model's input size
+    # input_image = cv2.resize(image, input_shape)
+    # Convert image to RGB
+    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+    # Normalize and convert to tensor
+    input_image = input_image / 255.0
+    input_image = np.transpose(input_image, (2, 0, 1)).astype(np.float32)
+    input_image = np.expand_dims(input_image, axis=0)
+    return input_image
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    results = model(frame, stream=True)
+    for result in results:
+        cv2.imshow("Card-Vision", result.plot())
+    
+    # cv2.imshow("ffff", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
--- cv-model-trainer.py
+++ cv-model-trainer.py
@@ -3,10 +3,11 @@ from ultralytics import YOLO
 def train():
 
     # Load a model
-    model = YOLO("runs/detect/train3/weights/best.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("runs/detect/card-vision/weights/best.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="datasets/Card-Detection-11/poker-data.yaml", epochs=10, imgsz=640, cache="ram", batch=16, device=0, amp=False, workers=4)  # train the model
+    model.train(data="datasets/Card-Detection-12/data.yaml", name="card-vision", exist_ok=True, epochs=10, 
+                imgsz=640, cache="ram", batch=16, device=0, amp=False, workers=4)  # train the model
     # metrics = model.val()  # evaluate model performance on the validation set
     # path = model.export(format="onnx")  # export the model to ONNX format
 
